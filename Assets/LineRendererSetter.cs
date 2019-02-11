58ecf6716423b548ded4e19fb2e2d1fb1ee4828c
Chan <cliwo@naver.com>
2019-02-11 17:23:00 +0900

    벽을 Frame 으로 대체

---
+++ Assets/LineRendererSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererSetter : MonoBehaviour {
+
+	public LineRenderer lR;
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(new Vector3(0, Particle.widthHalf, 0),
+			new Vector3(Particle.width,Particle.width,Particle.width));
+		
+	}
+}
+
+		// Vector3 BLN, BLF, BRN, BRF;
+		// Vector3 TLN, TLF, TRN, TRF;
+
+		// BLN = new Vector3(-Particle.widthHalf,0,-Particle.widthHalf);
+		// BLF = new Vector3(-Particle.widthHalf,0,Particle.widthHalf);
+		// BRN = new Vector3(Particle.widthHalf,0,-Particle.widthHalf);
+		// BRF = new Vector3(Particle.widthHalf,0,Particle.widthHalf);
+
+		// TLN = new Vector3(-Particle.widthHalf,Particle.width,-Particle.widthHalf);
+		// TLF = new Vector3(-Particle.widthHalf,Particle.width,Particle.widthHalf);
+		// TRN = new Vector3(Particle.widthHalf,Particle.width,-Particle.widthHalf);
+		// TRF = new Vector3(Particle.widthHalf,Particle.width,Particle.widthHalf);
