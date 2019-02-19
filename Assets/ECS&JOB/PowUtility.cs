3c964442d79d22014248048a94e0d36189ac2e61
Chan <cliwo@naver.com>
2019-02-19 18:22:27 +0900

    Pow Util Bug 수정

---
+++ Assets/ECS&JOB/PowUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PowUtility {
+
+	public static float IntPow(float value, int val)
+	{
+		float result = value;
+		for(int i = 0 ; i < val-1 ; i++)
+		{
+			result *= value;
+		}
+		return result;
+	}
+
+}
